ccebc29b3f79e0684c5ff916b949929fe1f75826
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-04-28 17:30:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function getCaloriesFromInputs(list) {
   let calories = 0;
   
   for (const item of list) {
-    const currVal = item.value;
+    const currVal = cleanInputString(item.value);
   }
 }
 addEntryButton.addEventListener('beforeend', HTMLString);
