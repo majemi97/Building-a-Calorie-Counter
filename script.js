def7165a2fd7098b99ce2d9f79eac97e07319b4b
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-04-29 15:34:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -84,5 +84,9 @@ function getCaloriesFromInputs(list) {
   return calories;
 }
 
+function clearForm() {
+  
+}
+
 addEntryButton.addEventListener("click", addEntry);
 calorieCounter.addEventListener("submit", calculateCalories);
