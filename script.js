f24fde0ace56ae36f64a9ade63adb3681fbbb11a
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-04-29 13:28:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -44,6 +44,10 @@ function calculateCalories(e) {
 
   const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
   const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+  const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+  const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+  const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+
 }
 
 function getCaloriesFromInputs(list) {
