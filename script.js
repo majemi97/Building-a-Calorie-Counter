0aaaf1b23dc524aee6a101c6b52f7a14f9674b65
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-04-29 13:57:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -55,8 +55,8 @@ function calculateCalories(e) {
 
   const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
   const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
-  const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit";
-
+  const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+  output.innerHTML = ``;
 }
 
 function getCaloriesFromInputs(list) {
